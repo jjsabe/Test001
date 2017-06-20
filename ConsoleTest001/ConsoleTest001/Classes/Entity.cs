ad5341403585a35ba3cbe78fa4e62ac9a9f3deb6
Jordi Jiménez <jjsabe@farmalinked.com>
2017-06-20 18:19:03 +0200

    Añado Class Entity a Branch 2

---
+++ ConsoleTest001/ConsoleTest001/Classes/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest001.Classes
+{
+    internal class Entity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string VATCode { get; set; }
+    }
+}
