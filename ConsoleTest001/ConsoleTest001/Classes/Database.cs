d0de12fd8fafb5187af5e7a6bc40e9cec18c49cc
Jordi Jiménez <jjsabe@farmalinked.com>
2017-06-20 10:37:40 +0200

    First test

---
+++ ConsoleTest001/ConsoleTest001/Classes/Database.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest001.Classes
+{
+    internal class Database
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
---
+++ ConsoleTest001/ConsoleTest001/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Classes.Database db = new Classes.Database();
+            db.Id = 1;
+            db.Name = "test";
+
+        }
+    }
+}
